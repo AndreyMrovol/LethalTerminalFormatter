7a3d16d70f40373ca4a15f0d08156b2467334dc7
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2024-07-30 10:22:00 +0200

    LLL postfix LoadNewNode replacement will compare TF's replaced node

--- TerminalFormatter/Compatibility/LethalLevelLoader.cs
+++ TerminalFormatter/Compatibility/LethalLevelLoader.cs
@@ -13,6 +13,19 @@ namespace TerminalFormatter
     {
         public static void Init()
         {
+            Plugin.harmony.Patch(
+                AccessTools.Method(
+                    typeof(LethalLevelLoader.Patches),
+                    "TerminalLoadNewNode_Postfix"
+                ),
+                prefix: new HarmonyMethod(typeof(LLLCompatibility), nameof(LLLLoadNodePatch))
+            );
+
+            GetLLLSettings();
+        }
+
+        public static void GetLLLSettings()
+        {
             // try casting the configManager entry value to PreviewInfoType enum
             // if it fails, set the value to the default enum value
 
@@ -47,6 +60,58 @@ namespace TerminalFormatter
             }
 
             Variables.ISLLLActive = true;
+        }
+
+        public static bool LLLLoadNodePatch(Terminal __0, ref TerminalNode __1)
+        {
+            if (__1 == Variables.LastReplacedNode)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static string InvokeMoonOverrideInfoEvent(
+            ExtendedLevel extendedLevel,
+            PreviewInfoType infoType
+        )
+        {
+            // Get the type of the TerminalManager class
+            Type terminalManagerType = typeof(TerminalManager);
+
+            // Get the event info for the onBeforePreviewInfoTextAdded event
+            EventInfo eventInfo = terminalManagerType.GetEvent(
+                "onBeforePreviewInfoTextAdded",
+                BindingFlags.Static | BindingFlags.Public
+            );
+
+            // Get the field that stores the delegate for the event (events are backed by delegate fields)
+            FieldInfo fieldInfo = terminalManagerType.GetField(
+                "onBeforePreviewInfoTextAdded",
+                BindingFlags.Static | BindingFlags.NonPublic
+            );
+
+            // Get the delegate from the field
+            if (fieldInfo != null)
+            {
+                var delegateInstance = fieldInfo.GetValue(null) as Delegate;
+
+                if (delegateInstance != null)
+                {
+                    // Create an array of parameters for the delegate invocation
+                    object[] parameters = new object[] { extendedLevel, infoType };
+
+                    // Iterate through the invocation list and invoke each subscriber
+                    foreach (var handler in delegateInstance.GetInvocationList())
+                    {
+                        var result = handler.DynamicInvoke(parameters);
+                        return result as string;
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
--- TerminalFormatter/Variables.cs
+++ TerminalFormatter/Variables.cs
@@ -16,6 +16,8 @@ namespace TerminalFormatter
 
         public static Terminal Terminal;
 
+        public static TerminalNode LastReplacedNode = null;
+
         public static bool IsACActive = false;
         public static bool ISLLLActive = false;
 
