3b489e2d7c99372027f3401acbb8265303588c95
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2026-06-22 23:49:08 +0200

    redid the moon catalogue to not be shit

--- TerminalFormatter/Nodes/Moons.cs
+++ TerminalFormatter/Nodes/Moons.cs
@@ -1,12 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Text;
-using System.Text.RegularExpressions;
-using HarmonyLib;
 using MrovLib;
-using TerminalFormatter.Compatibility;
 using TerminalUtils;
 using TerminalUtils.Definitions;
 using UnityEngine;
@@ -21,23 +16,37 @@ namespace TerminalFormatter.Nodes
       this.HelpText = " Welcome to the exomoons catalogue! \n Use ROUTE to to set the autopilot route. \n Use INFO to learn about any moon.";
     }
 
-    public override bool IsNodeValid(TerminalNode node)
-    {
-      return true;
-    }
-
     public override string GetNodeText(TerminalNode node)
     {
-      bool decor = ConfigManager.ShowDecorations.Value;
-
-      List<SelectableLevel> levelsToDisplay = TerminalUtils.TerminalManager.GetCurrentLevels();
-
+      // TODO: change this to TerminalManager.GetCurrentLevels() after the update upstream
+      List<SelectableLevel> levelsToDisplay = TerminalManager.GetCurrentLevels();
       List<PreviewInfoType<SelectableLevel>> currentPreviewTypes = TerminalManager.CurrentPreviewInfoType;
 
-      var table = new ConsoleTables.ConsoleTable(TerminalUtils.TerminalManager.CurrentPreviewInfoType.Select(info => info.Name).ToArray());
+      if (ConfigManager.ShowNumberedPlanetNames.Value)
+      {
+        currentPreviewTypes = currentPreviewTypes
+          .Select(infoType =>
+          {
+            if (infoType.Name == "Name")
+            {
+              return TerminalUtils.TerminalManager.PreviewInfoTypes["NumberedName"];
+            }
+            else
+            {
+              return infoType;
+            }
+          })
+          .ToList();
+      }
 
-      var tableInConsole = new ConsoleTables.ConsoleTable("Name", "Price", "Weather", "Difficulty");
-
+      var table = new ConsoleTables.ConsoleTable(
+        TerminalUtils
+          .TerminalManager.CurrentPreviewInfoType.Select(info =>
+          {
+            return "";
+          })
+          .ToArray()
+      );
       var adjustedTable = new StringBuilder();
 
       string headerName = "MOONS CATALOGUE";
@@ -46,10 +55,10 @@ namespace TerminalFormatter.Nodes
         {
           {
             0,
-            $"<size=50%>PREVIEW: {string.Join(", ", TerminalUtils.TerminalManager.CurrentPreviewInfoType.Where(info => !info.Name.Contains("Name")).Select(info => info.Name.ToUpper()))}</size>"
+            $"PREVIEW: <size=70%>{string.Join(", ", TerminalUtils.TerminalManager.CurrentPreviewInfoType.Where(info => !info.Name.Contains("Name")).Select(info => info.Name.ToUpper()))}</size>"
           },
-          { 1, $"<size=50%>SORT: {TerminalUtils.TerminalManager.CurrentSortInfoType.Name.ToUpper()}</size>" },
-          { 2, $"<size=50%>FILTER: {TerminalUtils.TerminalManager.CurrentFilterInfoType.Name.ToUpper()}</size>" },
+          { 1, $"SORT: {TerminalUtils.TerminalManager.CurrentSortInfoType.Name.ToUpper()}" },
+          { 2, $"FILTER: {TerminalUtils.TerminalManager.CurrentFilterInfoType.Name.ToUpper()}" },
         };
       string moonsHeader = new Header().CreateNumberedHeader(headerName, 2, headerInfo);
 
@@ -57,32 +66,41 @@ namespace TerminalFormatter.Nodes
 
       foreach (SelectableLevel level in levelsToDisplay)
       {
-        if (LevelHelper.IsHidden(level))
+        if (LevelHelper.IsHidden(level) && !ConfigManager.AlwaysDisplayHiddenMoons.Value)
         {
           continue;
         }
 
-        string planetName = MrovLib.StringResolver.GetAlphanumericName(level);
-        string numbersInPlanetName = Regex.Match(level.PlanetName, @"^\d+").Value;
-
-        // if the number is shorter than 3, fill it with 0s from the left
-        if (ConfigManager.ShowNumberedPlanetNames.Value)
-        {
-          planetName = $"{numbersInPlanetName.PadLeft(3, '0')} {planetName}";
-        }
-
-        if (decor)
-        {
-          planetName = $"* {planetName}";
-        }
-
-        string[] rowItems = currentPreviewTypes.Select(info => info.Value(level)).ToArray();
+        string[] rowItems = currentPreviewTypes
+          .Select(info =>
+          {
+            if (info.Name.Contains("Name"))
+            {
+              return $"* {info.Value(level)}";
+            }
+            else
+            {
+              return info.Value(level);
+            }
+          })
+          .ToArray();
         table.AddRow(rowItems);
 
         if (itemCount % 3 == 0)
         {
           itemCount = 1;
-          table.AddRow(currentPreviewTypes.Select(_ => "").ToArray());
+
+          if (ConfigManager.ShowGroupDividerLines.Value)
+          {
+            var dividerRow = new string[currentPreviewTypes.Count];
+            dividerRow[0] = "".PadRight(Settings.planetNameWidth, '-');
+
+            table.AddRow(dividerRow);
+          }
+          else
+          {
+            table.AddRow(currentPreviewTypes.Select(_ => "").ToArray());
+          }
         }
         else
         {
@@ -98,19 +116,12 @@ namespace TerminalFormatter.Nodes
         adjustedTable.Append(this.HelpText != null ? $"\n{this.HelpText}\n\n" : "");
       }
 
-      // adjustedTable.Append(
-      //   LethalLevelLoader.Settings.levelPreviewFilterType == FilterInfoType.Tag
-      //     ? " If you enabled tag filtering by accident: \n Use the <color=#0be697>filter none</color> command to disable it.\n\n"
-      //     : ""
-      // );
-
       adjustedTable.Append($" The Company // Buying at {Mathf.RoundToInt(StartOfRound.Instance.companyBuyingRate * 100f)}% \n\n");
 
       adjustedTable.Append(tableString);
 
       string finalString = adjustedTable.ToString().TrimEnd();
 
-      Plugin.debugLogger.LogDebug("All strings:\n" + tableInConsole.ToMinimalString());
       Plugin.debugLogger.LogDebug("Final string:\n" + finalString);
 
       return finalString;
