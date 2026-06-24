730a757961c0c8eef52a380a001f133c6907218f
AndreyMrovol <32998721+AndreyMrovol@users.noreply.github.com>
2026-06-24 08:46:15 +0200

    ide screamed at me for not doing this

--- TerminalFormatter/Patches/String.cs
+++ TerminalFormatter/Patches/String.cs
@@ -5,11 +5,11 @@ namespace TerminalFormatter.Patches
 {
   public static class String
   {
-    public static string Sanitized(this string currentString) => new string(currentString.SkipToLetters().RemoveWhitespace().ToLowerInvariant());
+    public static string Sanitized(this string currentString) => new(currentString.SkipToLetters().RemoveWhitespace().ToLowerInvariant());
 
-    public static string RemoveWhitespace(this string input) => new string(input.ToCharArray().Where(c => !Char.IsWhiteSpace(c)).ToArray());
+    public static string RemoveWhitespace(this string input) => new(input.ToCharArray().Where(c => !Char.IsWhiteSpace(c)).ToArray());
 
-    public static string SkipToLetters(this string input) => new string(input.SkipWhile(c => !char.IsLetter(c)).ToArray());
+    public static string SkipToLetters(this string input) => new(input.SkipWhile(c => !char.IsLetter(c)).ToArray());
 
     public static string StripSpecialCharacters(this string input)
     {
@@ -17,7 +17,7 @@ namespace TerminalFormatter.Patches
 
       foreach (char charmander in input)
       {
-        if ((char.IsLetterOrDigit(charmander)) || charmander.ToString() == " ")
+        if (char.IsLetterOrDigit(charmander) || charmander.ToString() == " ")
         {
           returnString += charmander;
         }
